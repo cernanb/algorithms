2c53e8eea7bb4df8ac5f100711ae85b5ea348da4
humaiarad <d.humaiara@gmail.com>
2026-07-07 21:05:17 -0400

    adding pbjalgorithm

--- algorithms.js
+++ algorithms.js
@@ -1,7 +1,7 @@
-const algorithms = [
-    "prepare bread", 
-    "spread peanutbutter", 
-    "spread jam",
-    "slice the bread",
+const pbjalgorithms = [
+    "prepare 2 slices of bread", 
+    "spread peanutbutter on one slice of bread", 
+    "spread jam on the other slice of bread",
+    "slice the bread in half",
     "serve the sandwich",
 ];
