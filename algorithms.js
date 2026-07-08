5690f64c669eeda9a0e70374e32eb1920b81794d
Cernan Bernardo <cernanb@gmail.com>
2026-07-07 17:41:53 -0700

    add pbj algo to file

--- algorithms.js
+++ algorithms.js
@@ -1,1 +1,10 @@
 const algorithms = [];
+
+const pbjAlgo = [
+  "Get bread",
+  "Get peanut butter",
+  "Get jelly",
+  "Spread peanut butter on one slice of bread",
+  "Spread jelly on the other slice of bread",
+  "Put the two slices of bread together",
+];
